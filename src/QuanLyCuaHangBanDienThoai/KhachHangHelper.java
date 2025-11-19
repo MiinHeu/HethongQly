27c8224c482da5e25f8e87326c7a7f50f1d416db
dungnguyen47 <tridungsinh@gmail.com>
2025-11-19 23:04:09 +0700

    KhachHangHelper.java

--- src/QuanLyCuaHangBanDienThoai/KhachHangHelper.java
+++ src/QuanLyCuaHangBanDienThoai/KhachHangHelper.java
@@ -1,18 +1,18 @@
 package QuanLyCuaHangBanDienThoai;
 
 class KhachHangHelper {
-    private static int dem = 0;
+
+    private static int stt = 1;
 
     public static String sinhMaKhachHang() {
-        dem++;
-        return String.format("KH%03d", dem);
+        return String.format("KH%03d", stt++);
     }
 
     public static boolean validateSDT(String sdt) {
-        return sdt.matches("\\d{10,11}");
+        return sdt.matches("0\\d{9}");
     }
 
     public static boolean validateEmail(String email) {
-        return email.matches("^[\\w.-]+@[\\w.-]+\\.[a-z]{2,}$");
+        return email.matches("^[\\w.-]+@[\\w.-]+\\.[A-Za-z]{2,6}$");
     }
 }
